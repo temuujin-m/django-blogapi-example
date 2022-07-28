8e16c9a995c3616e69d95eeb4cb94796c18718c5
temuujin-m <jrocktemka@gmail.com>
2022-07-28 12:13:57 +0900

    add environment variables

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'dj_rest_auth',
     'dj_rest_auth.registration',
+    'drf_spectacular',
     #local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
@@ -152,6 +153,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 CORS_ORIGIN_WHITELIST = (
@@ -160,3 +162,15 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Blog API Project",
+    "DESCRIPTION": "A sample blog to learn about DRF",
+    "VERSION": "1.0.0",
+}
+
+from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
